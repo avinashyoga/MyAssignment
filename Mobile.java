dfe20bd9c0953fe9a892ae6d09cc11816b90f5fe
avinashyoga <139771671+avinashyoga@users.noreply.github.com>
2023-07-18 22:31:31 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,22 @@
+package Week1.Day2;
+
+public class Mobile {
+	public static void main(String[] args) {
+		
+	
+	String mobileName = "Oneplue";
+	System.out.println("Brand :" + mobileName);
+	String mobileLogo = "1+ Plus";
+	System.out.println("Logo :"+ mobileLogo);
+	int modelNumber = 979054678;
+	System.out.println("Model Number-" + modelNumber);
+	long mobileImeiNumber = 789652447368745L;
+	System.out.println("IMEI Number-" + mobileImeiNumber);
+	float mobilePrice = 25000.52f;
+	System.out.println("Price- Rs."+ mobilePrice);
+	boolean isDamaged = false;
+	System.out.println("Failure-"+ isDamaged);
+	
+
+	}
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,22 @@
+package Week1.Day2;
+
+public class TwoWheeler {
+public static void main(String[] args) {
+		
+		int Wheels = 2;
+		System.out.println("Wheels :" + Wheels );
+		short Mirrors = 2;
+		System.out.println("Mirrors :" + Mirrors);
+		long chassisNumber = 98765432345L;
+		System.out.println("Chassis Number :" + chassisNumber);
+		String bikeName = "Pulsar NS-200";
+		System.out.println("BilkeNAme :" + bikeName);
+		int runningKM = 35 ;
+		System.out.println("KM/Hour :" + runningKM);
+
+
+	}
+
+}
+
+
